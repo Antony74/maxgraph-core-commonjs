b52f4c2383428abfec116359b2f4490d941869cb
Antony74 <akb@akb.me.uk>
2025-04-26 13:18:12 +0100

    built 0.17.0

--- dist/index.js
+++ dist/index.js
@@ -45483,15 +45483,14 @@ const inlineImage = async (image) => {
     isFile = url.protocol === "file:";
   } catch (_e) {
   }
+  let buffer;
   if (isFile) {
-    path = path.slice(7);
+    buffer = await fsp.readFile(path.slice(7));
   } else {
-    throw new Error(
-      `Can't inline "${path}".  Currently only file:// urls are supported.`
-    );
+    const response = await fetch(path);
+    buffer = await response.arrayBuffer();
   }
-  const buffer = await fsp.readFile(path);
-  const content = buffer.toString("base64");
+  const content = Buffer.from(buffer).toString("base64");
   const mimeType = mime.lookup(path);
   const contentType = mimeType ? mimeType : "";
   const newUrl = `data:${contentType};base64, ${content}`;
